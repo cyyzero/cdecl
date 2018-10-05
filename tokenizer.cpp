e621bc125ad23114e5fac1c0d2b22194d84d519d
cyyzero <cyyzero16@qq.com>
2018-10-05 23:49:29 +0800

    Add tokenzier decleration

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,3 +1,2 @@
 #include <string>
 #include "tokenizer.h"
-
--- tokenizer.h
+++ tokenizer.h
@@ -6,12 +6,31 @@
 class Tokenizer
 {
 public:
-    Tokenizer(const std::string& code)
-        : m_code(code) { }
+    enum Token_kind: uint8_t
+    {
+        Number, Identifier, Type,
+        Address , LCurly, RCurly, LSquare, RSquare, LBraket, RBraket, Star, Comma,
+        End
+    };
+    using Token = std::pair<Token_kind, std::string>;
+public:
+    Tokenizer(const std::string& line)
+        : m_line(line)
+    {
+        m_current = m_line.begin();
+        next();
+    }
+
     ~Tokenizer() = default;
 
+    Token get();
+    void next();
+    void reset();
 private:
-    const std::string& m_code;
+    //std::istream& m_file;
+    Token m_token;
+    std::string m_line;
+    std::string::iterator m_current;
 };
 
 #endif
